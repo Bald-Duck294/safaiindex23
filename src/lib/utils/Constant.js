3194856c6bc2f8dbc632fbef0fec53024a4ac7a9
Goolu Moolu <goolumoolu294@gmail.com>
2025-07-31 18:38:18 +0530

    "update api-base-url second"

--- src/lib/utils/Constant.js
+++ src/lib/utils/Constant.js
@@ -1,5 +1,5 @@
 
 // const API_BASE_URL = "http://localhost:8000/api";
-const API_BASE_URL = "https://safai-index-backend.onrender.com/"
+const API_BASE_URL = "https://safai-index-backend.onrender.com/api"
 
 export default API_BASE_URL;
